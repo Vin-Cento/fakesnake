00185ea343c56c4861c04103253e5a6c79bb093e
Vincent <vincentliu362@gmail.com>
2023-10-22 19:35:19 -0500

    doc: update pypi

--- setup.py
+++ setup.py
@@ -1,2 +1,9 @@
 from setuptools import setup
-setup()
+
+# read the contents of your README file
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(long_description=long_description, long_description_content_type="text/markdown")
