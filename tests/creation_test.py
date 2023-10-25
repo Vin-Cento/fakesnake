e626da01ec9df089a5506275045a8502b357674a
Vincent <vincentliu362@gmail.com>
2023-10-24 23:17:43 -0500

    fix: process a long file in chunks

--- tests/creation_test.py
+++ tests/creation_test.py
@@ -2,13 +2,13 @@ from fakesnake.creation import *
 
 
 def test_passwords_num():
-    passwords = create_passwords(5, 15, 25)
-    assert len(passwords) == 5
+    passwords = create_passwords(5, 15, 25, "password")
+    assert len(passwords) == 6
 
 
 def test_passwords_error():
     try:
-        create_passwords(1, 1, 1)
+        create_passwords(1, 1, 1, "password")
         assert False
     except AssertionError:
         assert True
