2b1e483d1d3f29fb8eca0f30df9a6a79685f0e1f
Vincent <vincentliu362@gmail.com>
2024-03-26 22:24:08 -0500

    test: describe relationship table

--- tests/describe_table_handler_test.py
+++ tests/describe_table_handler_test.py
@@ -20,11 +20,24 @@ def test_missing_table():
     assert result.exit_code == 0
 
 
-# TODO: Relationship Test
-# def test_relationship_table():
-#     runner = CliRunner()
-#     result = runner.invoke(database, ["describe", "users"])
-#     print("")
-#     print(result.output.split('Relationship'))
-#     assert result.output != "Table not found\n"
-#     assert result.exit_code == 0
+def test_relationship_table():
+    runner = CliRunner()
+    # create student table
+    runner.invoke(
+        database, ["exec", "create table student (id serial primary key, name text)"]
+    )
+    # create class table with foreign key to student
+    runner.invoke(
+        database,
+        [
+            "exec",
+            "create table class (id serial primary key, class text,student_id integer references student(id))",
+        ],
+    )
+    runner.invoke(database, ["insert", "student"])
+    runner.invoke(database, ["insert", "class"])
+    result = runner.invoke(database, ["describe", "users"])
+    print("")
+    print(result.output)
+    assert result.output != "Table not found\n"
+    assert result.exit_code == 0
